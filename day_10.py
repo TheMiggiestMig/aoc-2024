c3faa1f59f7e4fdcb3b212d4c0242bdca06f001b
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2024-12-13 08:48:43 +0930

    completed day 10 part A and B
    Funny enough, i solved part B before part A because i misread it.

--- day_10.py
+++ day_10.py
@@ -1,4 +1,5 @@
-test = 1
+test = 0
+
 file = 'test' if test else 'data'
 
 solve_a = 0
@@ -28,19 +29,27 @@ with open(file) as f:
 
 bounds = (bounds_x, bounds_y)
 
-def traverse_all_paths(x:int, y:int, height=0):
+trail_path = {}
+
+def traverse_all_paths(x:int, y:int, height=0, start=None):
     score = 0
-    if height == 9: return 1
+    if height == 9:
+        trail_path[(start, (x, y))] = True
+        return 1
 
     for direction in directions:
         next_cell = (x + direction[0], y + direction[1])
         if 0 <= next_cell[0] < bounds[0] and 0 <= next_cell[1] < bounds[1] and map_grid[next_cell[1]][next_cell[0]] == height + 1:
-            score += traverse_all_paths(*next_cell, height + 1)
+            score += traverse_all_paths(*next_cell, height + 1, start)
     
     return score
 
 for y, row in enumerate(map_grid):
     for x, height in enumerate(row):
-        solve_a += traverse_all_paths(x, y, height)
+        if height == 0:
+            solve_b += traverse_all_paths(x, y, height, (x, y))
+
+solve_a = len(trail_path)
 
 print(solve_a)
+print(solve_b)
