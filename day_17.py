a6728d98031e0c5a354c641a8c2cda17a2797820
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2024-12-20 15:22:28 +0930

    Create day_17.py

---
+++ day_17.py
@@ -0,0 +1,25 @@
+import io
+test = 1
+test_data = io.StringIO(
+"""
+Put your test data here
+This is more data
+This is a third line
+"""
+)
+test_data.readline() # Get rid of the first empty line (it's only there for display purposes).
+
+solve_a = 0
+solve_b = 0
+expected_a = 420
+expected_b = 9001
+
+# Parse the input and provide as many outputs as required.
+with test_data if test else open("data") as file:
+    for line in file:
+        print(line.strip())
+
+# Do the magic here
+
+print(f"[ DEBUG ] {f'PASS! ({solve_a})' if solve_a == expected_a else f'Fail (expected {expected_a}, got {solve_a}).'}" if test else solve_a)
+print(f"[ DEBUG ] {f'PASS! ({solve_b})' if solve_b == expected_b else f'Fail (expected {expected_b}, got {solve_b}).'}" if test else solve_b)
