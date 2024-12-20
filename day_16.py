a97c868ffabc575a4576b6ae2c9b0515dfed71bf
TheMiggiestMig <89686727+TheMiggiestMig@users.noreply.github.com>
2024-12-20 15:17:53 +0930

    Update day_16.py part B solved, messy af

--- day_16.py
+++ day_16.py
@@ -1,6 +1,4 @@
 import io
-import heapq
-import time
 
 test = 0
 test_data = []
@@ -61,38 +59,20 @@ directions = {
     (-1, 0): [(0, 1), (0, -1)],
     (0, 1): [(-1, 0), (1, 0)]
 }
-start_position, end_position = (0, 0), (0, 0)
-map_grid = []
-bounds = (0, 0)
-
-with test_data[test - 1] if test else open("data") as file:
-    rows = 0
-    for line in file:
-        row = [1]*len(line)
-        for cols, cell in enumerate(line):
-            if cell == "#": row[cols] = 0
-            elif cell == "S": start_position = (cols, rows)
-            elif cell == "E": end_position = (cols, rows)
-        map_grid.append(row)
-        rows += 1
-    
-    bounds = (len(map_grid[0]), rows)
 
 class Cell():
     x = 0
     y = 0
     direction = (1, 0)
     total_cost = 0
-    parent = None
-    visited_cells = []
+    parents = []
 
-    def __init__(self, total_cost=0, x=0, y=0, direction=(1, 0), parent=None, visited_cells=[]):
-        self.total_cost = total_cost
+    def __init__(self, x=0, y=0, total_cost=0, direction=(1, 0), parents=[]):
         self.x = x
         self.y = y
+        self.total_cost = total_cost
         self.direction = direction
-        self.parent = parent
-        self.visited_cells = visited_cells
+        self.parents = parents
     
     def diff(self, pos):
         x, y = (0, 0)
@@ -121,49 +101,65 @@ class Cell():
     
     pos = property(_get_pos, _set_pos)
 
+# Parse the input data
+start_position, end_position = (0, 0), (0, 0)
+map_grid = []
+bounds = (0, 0)
+
+with test_data[test - 1] if test else open("data") as file:
+    rows = 0
+    for line in file:
+        row = [1]*len(line)
+        for cols, cell in enumerate(line):
+            if cell == "#":row[cols] = 0
+            elif cell == "S": start_position = (cols, rows)
+            elif cell == "E": end_position = (cols, rows)
+        map_grid.append(row)
+        rows += 1
+    
+    bounds = (len(map_grid[0]), rows)
+
 def in_bounds(cell:tuple, bounds:tuple):
     return 0 <= cell[0] < bounds[0] and 0 <= cell[1] < bounds[1]
 
+def backtrack(cell, path=[]):
+    path.append(cell.pos)
+    for cell_parent in cell.parents:
+        path.append(cell_parent.pos)
+        backtrack(cell_parent, path)
+    return path
+
 # Do the magic here
-def solve(start, end, map_grid, step_cost=1, turn_cost=1000):
+def solve(start, end, map_grid):
     step = 0
-    pos = start
     previous_cell = None
     
-    check_cells = [Cell(0, *start)]
-
-    best_path_cells = set()
-    best_cost = 0
+    check_cells = [start]
+    visited_cells = {}
 
     while check_cells:
-        step += 1
-        if not step % 1000:
-            print(step, len(check_cells), f"Best cost ({best_cost})")
-            time.sleep(0.001)
-            
         cell = check_cells.pop(0)
         
         # Check if we've reached the end.
-        if cell.pos == end and not best_cost:
-            path = []
-            best_cost = cell.total_cost
-        
-        if best_cost:
-            if cell.total_cost == best_cost:
-                best_cell = cell
-                while best_cell.parent:
-                    best_path_cells.add(best_cell.pos)
-                    best_cell = best_cell.parent
-            elif cell.total_cost > best_cost:
-                continue
+        if cell.pos == end:
+            return (cell.total_cost, set(backtrack(cell)))
                 
         # Add the cells to check.
         for direction in [cell.direction] + directions[cell.direction]:
             check_pos = cell.add(direction)
             
-            if in_bounds(check_pos, bounds) and map_grid[check_pos[1]][check_pos[0]] and check_pos not in cell.visited_cells:
+            if in_bounds(check_pos, bounds) and map_grid[check_pos[1]][check_pos[0]]:
                 check_cost = cell.total_cost + step_cost + (0 if direction == cell.direction else turn_cost)
-                check_cell = Cell(check_cost, *check_pos, direction, cell, cell.visited_cells + [cell.pos])
+                check_cell = Cell(*check_pos, check_cost, direction, [cell])
+                
+                if (check_pos, direction) in visited_cells:
+                    if visited_cells[(check_pos, direction)].total_cost < check_cost:
+                        continue
+                    elif visited_cells[(check_pos, direction)].total_cost == check_cost:
+                        visited_cells[(check_pos, direction)].parents.append(cell)
+                        continue
+                else:
+                    visited_cells[check_pos, direction] = check_cell
                 
                 inserted = False
                 for i in range(len(check_cells)):
@@ -171,18 +167,16 @@ def solve(start, end, map_grid, step_cost=1, turn_cost=1000):
                         check_cells.insert(i, check_cell)
                         inserted = True
                         break
-
                 if not inserted: check_cells.append(check_cell)
-
-    return (best_cost, best_path_cells)
-
-solve_a, path = solve(start_position, end_position, map_grid)
-solve_b = len(path) + 1
+            
+    return (-1, [])
+solve_a, best_path = solve(Cell(*start_position), end_position, map_grid)
+solve_b = len(best_path)
 
 for y, row in enumerate(map_grid):
     row_str = ""
     for x, cell in enumerate(row):
-        row_str += "." if (x, y) in path else " " if cell else "#"
+        row_str += "." if (x, y) in best_path else " " if cell else "#"
     print(row_str)
 
 print(f"[ DEBUG ] {f'PASS! ({solve_a})' if solve_a == expected_a[test - 1] else f'Fail (expected {expected_a[test - 1]}, got {solve_a}).'}" if test else solve_a)
